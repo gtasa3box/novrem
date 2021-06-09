6dc0a5a5b3ef0014fd394bf3c9d3d2577b875e76
gtasa3box <gtasa3box@gmail.com>
2021-06-09 07:00:33 +0300

    two commit

--- app/index.js
+++ app/index.js
@@ -1,19 +1,18 @@
 import './index.scss';
 
-$('.main-slider').slick({
-	dots: true,
+$('.clients__slider').slick({
+	dots: false,
 	infinite: true,
 	speed: 300,
 	slidesToShow: 4,
-	slidesToScroll: 2,
+	slidesToScroll: 1,
 	responsive: [
 	{
 	  breakpoint: 1024,
 	  settings: {
 		slidesToShow: 3,
 		slidesToScroll: 3,
-		infinite: true,
-		dots: true
+		infinite: true
 	  }
 	},
 	{
