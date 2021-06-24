e071be742e94891c39f6d117a508b72251df41b0
gtasa3box <gtasa3box@gmail.com>
2021-06-24 15:49:54 +0300

    gay commit2

--- app/index.js
+++ app/index.js
@@ -1,5 +1,33 @@
 import './index.scss';
 
+
+window.onload= function() {
+        document.getElementById('burgerlink').onclick = function() {
+        toogleclass(this);
+        opennav('nav', this);
+        return false;
+    };
+};
+
+function opennav(id, click_burger) {
+    const div = document.getElementById(id);
+    if(div.style.display == 'flex') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.display = 'flex';
+    }
+};
+
+function toogleclass(click_burger) {
+	if (click_burger.classList.contains('active')) {
+		click_burger.classList.remove('active');
+	}
+	else {
+		click_burger.classList.add('active');
+	}
+};
+
 $('.clients__slider').slick({
 	dots: false,
 	infinite: true,
@@ -31,6 +59,5 @@ $('.clients__slider').slick({
 	}
   ]
 });
-
 
 console.log('Boilerplate is working!');
